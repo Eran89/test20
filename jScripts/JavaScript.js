85cf79481943b0d6d2300b13abeb1f510526f7d4
Eran89 <eran037@gmail.com>
2017-09-10 12:59:29 +0300

    initial commit

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $("#accordion").accordion({
+        collapsible: true
+    });
+});
+
+//$(function () {
+//    $("#accordion").accordion({
+//        heightStyle: "fill"
+//    });
+//});
+
+//$(function () {
+//    $("#accordion").accordion({
+//        heightStyle: "content"
+//    });
+//});
+//$(function () {
+//    $("#accordion").accordion();
+//});
+
